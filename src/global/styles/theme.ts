808c6328fbbd3eeba32eb36a264e971ec64bcf40
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-06-23 23:12:18 -0300

    Creation of global theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    primary: '#E51C44',
+    line: '#991F36',
+    secondary: '#495BCC',
+
+    background: '#0D133D',
+    shapesInputs: 'linear-gradient(180deg, #243189 0%, #1B2565 100%)',
+    
+    heading: '#DDE3F0',
+    body: '#ABB1CC',
+  }
+}
