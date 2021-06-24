bb8d0f36e253a4b9d09c0d3ed824dfbcde00b979
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-06-23 23:12:08 -0300

    Creation of button component

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+  TouchableOpacityProps,
+} from "react-native";
+
+import DiscordImg from "../../assets/discord.png";
+import { styles } from "./styles";
+
+interface ButtonIconProps extends TouchableOpacityProps {
+  title: string;
+}
+
+export function ButtonIcon({ title, ...rest }: ButtonIconProps) {
+  return (
+    <TouchableOpacity style={styles.container} {...rest}>
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon} />
+      </View>
+      <Text style={styles.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
