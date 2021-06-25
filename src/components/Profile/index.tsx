daa0ca43772dea11dd73c0c4e1ad186b52d30a2f
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-06-25 09:41:22 -0300

    Avatar component

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { View, Text } from "react-native";
 import { styles } from "./styles";
 
+import {Avatar} from '../Avatar';
+
 export function Profile() {
   return (
     <View style={styles.container}>
+
+      <Avatar urlImage="https://github.com/gustavocrvls.png" />
+
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá,</Text>
